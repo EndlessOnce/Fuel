d43c9d069ed617a5cc146ec0a75b7c557071bb57
John Mai <jprebornzer@gmail.com>
2020-03-05 12:49:11 -0600

    Added in the Forms Module for ngModel for two way binding for user input

--- Fuel-Quote/src/app/app.module.ts
+++ Fuel-Quote/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientProfileComponent } from './client-profile/client-profile.component';
@@ -12,7 +12,8 @@ import { ClientProfileComponent } from './client-profile/client-profile.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
