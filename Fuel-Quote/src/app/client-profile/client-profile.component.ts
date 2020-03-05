9afb3f9b1732cd11a5a54fcc2782ff6a50adc244
John Mai <jprebornzer@gmail.com>
2020-03-05 12:13:16 -0600

    Created the Client-Profile component

---
+++ Fuel-Quote/src/app/client-profile/client-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component(
+  {
+    // Allows us to use the component
+    selector: 'app-client-profile',
+    // A relative path that will point to when Angular parses in
+    templateUrl: './client-profile.component.html'
+  }
+)
+
+export class ClientProfileComponent
+{
+
+}
