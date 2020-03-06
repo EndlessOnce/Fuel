ecf57d46da6f8f2859d9234ee5a81d75656e564a
John Mai <jprebornzer@gmail.com>
2020-03-06 07:17:00 -0600

    Login / Sign up
    Login / Sign up finished

--- Fuel-Quote/src/app/app.module.ts
+++ Fuel-Quote/src/app/app.module.ts
@@ -3,19 +3,38 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatInputModule} from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { ClientProfileComponent } from './client-profile/client-profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './auth/login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from "@angular/common/http";
+import { SignupComponent } from './auth/signup/signup.component';
+
 // Always add in no components into the declartaions so that Angular knows what new components there are otherwise it won't read it at all
 @NgModule({
   declarations: [
     AppComponent,
-    ClientProfileComponent
+    ClientProfileComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
