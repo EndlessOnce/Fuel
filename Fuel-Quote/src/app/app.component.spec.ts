a03471551218831848bb4efb4c34ce92de55cea7
Kevin Vu <vukevin20@gmail.com>
2020-04-03 16:01:13 -0500

    Moved files for proper testing

--- Fuel-Quote/src/app/app.component.spec.ts
+++ Fuel-Quote/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Fuel-Quote');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Fuel-Quote app is running!');
-  });
 });
---
+++ Fuel-Quote/src/app/client-profile/client-profile.spec.ts
@@ -0,0 +1,7 @@
+import { ClientProfileComponent } from './client-profile.component';
+
+describe('CLientProfileCompoment', () => {
+  it('should create an instance', () => {
+    expect(new ClientProfileComponent()).toBeTruthy();
+  });
+});
