eb7c5cacb9106cef1b6ab84c1cdd4803c2c9db12
John Mai <jprebornzer@gmail.com>
2020-04-24 02:10:25 -0500

    Minor fix

--- Fuel-Quote/backend/app.js
+++ Fuel-Quote/backend/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 const userRoutes = require("./routes/user");
+
 const clientProfile = require("./routes/clientProfile");
 
 
@@ -47,6 +48,7 @@ app.use((req, res, next) => {
 // });
 
 app.use("/api/user", userRoutes);
+
 app.use("/api/client-profile", client-profile);
 
 
